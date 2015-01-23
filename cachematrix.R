ccf6bddbe1e589ff9026d8c7ea8347f1852a26d4
Honto Ming <honto.ming@gmail.com>
2015-01-23 15:17:53 -0800

    prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# honto-ming: http://ogp.me/ns/fb/honto-ming#a Programming in R - Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,24 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL 
+    ## a "re-constructor" where you erase the inverse and set the new 
+    ## values of x
+    set <- function(y) {   
+        x <<- y 
+        m <<- NULL
+    }
+    ## returns the original matrix
+    get <- function() x
+    # set the inverse stored in this function
+    setInverse <- function(inv) {inverse <<- inv}
+    # returns the inverse stored in this function
+    getInverse <- function() inverse
+    ## This is what the function makeCacheMatrix returns when called. So you can
+    ## use this vector as an "object" by making "calls" to the list elements
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
@@ -12,4 +29,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        # check to see if the makeVector "object," x, already has the mean cached
+        inv <- x$getInverse()
+        if(!is.null(inv)) {
+            # already cached. No need to perform calculations
+            message("getting cached data")
+            return(inv)
+        }
+        # Need to perform calculations
+        data <- x$get()
+        inv <- solve(data)
+        # set the mean value in makeVector "object"
+        x$setInverse(inv)
+        inv
 }
